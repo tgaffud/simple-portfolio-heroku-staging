9f9e0178e3a68a2abf72b26fa778ffb3025bb58d
Terence Gaffud <terence.gaffud@tuitt.com>
2020-09-22 16:05:54 +0800

    Reverted changes in Jumbo

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -14,7 +14,7 @@ export default function Jumbo(){
 		    extra attention to featured content or information.
 		  </p>
 		  <p>
-		    <Button variant="primary">Learn morer</Button>
+		    <Button variant="primary">Learn more</Button>
 		  </p>
 		</Jumbotron>
 	)
